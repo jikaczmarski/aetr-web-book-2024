18489072128566197b62e9e92124c13a751f115a
Elizabeth (Liz) Dobbins <eldobbins@alaska.edu>
2024-03-31 15:11:52 -0800

    use the main branch's commit time

--- .github/scripts/fix-timestamps.sh
+++ .github/scripts/fix-timestamps.sh
@@ -5,7 +5,7 @@
 # https://github.com/quarto-dev/quarto-cli/discussions/5899#discussioncomment-7673260
 
 git ls-tree -r --name-only HEAD | while read filename; do
-  unixtime=$(git log -1 --format="%at" -- "${filename}")
+  unixtime=$(git log main -1 --format="%at" -- "${filename}")
   touchtime=$(date -d @$unixtime +'%Y%m%d%H%M.%S')
   touch -t ${touchtime} "${filename}"
 done
