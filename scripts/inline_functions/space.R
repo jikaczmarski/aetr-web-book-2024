195932a3ddd5adcb478c737711225a50952075e6
ianalexmac <iamacdougall@alaska.edu>
2024-08-09 13:46:03 -0800

    update space function, spacing on pages

--- scripts/inline_functions/space.R
+++ scripts/inline_functions/space.R
@@ -2,10 +2,20 @@
 # To use: write `r space` outside of a code block
 # Modify globally at ./scripts/inline_functions/space.R
 
-space <- if (knitr::is_html_output()) {
-  "<br>"
-} else if (knitr::is_latex_output()) {
-  "\\vspace{1em}"  # Adjust the space as needed
-} else {
-  ""
+# space <- if (knitr::is_html_output()) {
+#   "<br>"
+# } else if (knitr::is_latex_output()) {
+#   "\\vspace{1em}"  # Adjust the space as needed
+# } else {
+#   ""
+# }
+
+space <- function(br = "<br>", vspace = "1em") {
+  if (knitr::is_html_output()) {
+    return(as.character(br))
+  } else if (knitr::is_latex_output()) {
+    return(as.character(paste0("\\vspace{", vspace, "}")))
+  } else {
+    return("")
+  }
 }
