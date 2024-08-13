173ec4586676c7e1c8a7d12d93d25c142f988383
ianalexmac <iamacdougall@alaska.edu>
2024-08-13 12:45:53 -0800

    reorganize R code

---
+++ scripts/R/theme_electrified.R
@@ -0,0 +1,9 @@
+theme_electrified <- function(base_size = 12, base_family = "Helvetica") {
+    theme_classic() +
+    theme(
+        legend.position = "top",
+        legend.title = element_blank(),
+        
+
+    )
+}
